94612b04385607300a926009250ab5b3505c0c8b
Sergei P. Vinogradov <serg.pvv@gmail.com>
2019-09-02 13:30:43 +0000

    fix tests

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -7,23 +7,23 @@ let diff;
 
 beforeEach(() => {
   path = `${__dirname}/__fixtures__/`;
-  diff = String(readFileSync(`${path}diff1`));
+  diff = String(readFileSync(`${path}diff`));
 });
 
 test('json', () => {
-  const before1 = `${path}before.json`;
-  const after1 = `${path}after.json`;
+  const before1 = `${path}before1.json`;
+  const after1 = `${path}after1.json`;
   expect(getDiff(before1, after1)).toBe(diff);
 });
 
 test('yaml', () => {
-  const before2 = `${path}before.yml`;
-  const after2 = `${path}after.yml`;
+  const before2 = `${path}before2.yml`;
+  const after2 = `${path}after2.yml`;
   expect(getDiff(before2, after2)).toBe(diff);
 });
 
 test('ini', () => {
-  const before3 = `${path}before.ini`;
-  const after3 = `${path}after.ini`;
+  const before3 = `${path}before3.ini`;
+  const after3 = `${path}after3.ini`;
   expect(getDiff(before3, after3)).toBe(diff);
 });
