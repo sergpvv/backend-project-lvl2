8d2810f394471834b7d57aea5c77aec79a6e6672
Sergei P. Vinogradov <serg.pvv@gmail.com>
2019-09-02 15:26:47 +0000

    fix issue

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -10,13 +10,14 @@ beforeEach(() => {
   diff = String(readFileSync(`${path}diff`));
 });
 
-test('json, yml', () => {
-  expect(getDiff(`${path}before1.json`, `${path}after1.json`)).toBe(diff);
-  expect(getDiff(`${path}before2.yml`, `${path}after2.yml`)).toBe(diff);
-});
-
-test('ini', () => {
-  const before3 = `${path}before3.ini`;
-  const after3 = `${path}after3.ini`;
-  expect(getDiff(before3, after3)).toBe(diff);
-});
+test.each([
+  ['.json', 'before1', 'after1'],
+  ['.yml', 'before2', 'after2'],
+  ['.ini', 'before3', 'after3']])(
+  '%s',
+  (ext, before, after) => {
+    const b = `${path}${before}${ext}`;
+    const a = `${path}${after}${ext}`;
+    expect(getDiff(b, a)).toBe(diff);
+  },
+);
--- src/parsers.js
+++ src/parsers.js
@@ -9,24 +9,25 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 
 const genDiff = (obj1, obj2) => {
-  const result = _.union(_.keys(obj1), _.keys(obj2)).reduce((acc, key) => {
-    const before = obj1[key];
-    const after = obj2[key];
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const result = keys.reduce((acc, key) => {
+    const before = `${key}: ${obj1[key]}\n`;
+    const after = `${key}: ${obj2[key]}\n`;
     const obj1HasKey = _.has(obj1, key);
     const obj2HasKey = _.has(obj2, key);
-    if (_.isEqual(before, after)) {
-      return `${acc}    ${key}: ${before}\n`;
-    }
+    let changes;
     if (obj1HasKey && obj2HasKey) {
-      return `${acc}  - ${key}: ${before}\n  + ${key}: ${after}\n`;
+      changes = _.isEqual(before, after)
+        ? `    ${before}`
+        : `  - ${before}  + ${after}`;
     }
-    if (obj1HasKey) {
-      return `${acc}  - ${key}: ${before}\n`;
+    if (obj1HasKey && !obj2HasKey) {
+      changes = `  - ${before}`;
     }
-    if (obj2HasKey) {
-      return `${acc}  + ${key}: ${after}\n`;
+    if (!obj1HasKey && obj2HasKey) {
+      changes = `  + ${after}`;
     }
-    return acc;
+    return `${acc}${changes}`;
   }, '{\n');
   return `${result}}\n`;
 };
